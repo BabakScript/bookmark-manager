dd40091d0a0bfa9b55e173f496d3c0b28ca5379a
Babak Haj Azim Zanjani <babak.hajazimzanjani@gmail.com>
2021-07-02 22:28:55 +0200

    feat: add createBookmark method

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { url } from 'inspector';
 import { Bookmark } from './bookmark.model';
 import { BookmarksService } from './bookmarks.service';
 
@@ -9,5 +10,10 @@ export class BookmarksController {
   @Get()
   findAll(): Bookmark[] {
     return this.bookmarksService.findAll();
+  }
+
+  @Post()
+  createBookmark(@Body('url') url, @Body('description') description): Bookmark {
+    return this.bookmarksService.createBookmark(url, description);
   }
 }
--- src/bookmarks/bookmarks.service.ts
+++ src/bookmarks/bookmarks.service.ts
@@ -4,15 +4,21 @@ import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class BookmarksService {
-  private bookmarks: Bookmark[] = [
-    {
-      id: uuid(),
-      url: 'http://google.com',
-      description: 'BabakScript Youtube Channel',
-    },
-  ];
+  private bookmarks: Bookmark[] = [];
 
   findAll(): Bookmark[] {
     return this.bookmarks;
+  }
+
+  createBookmark(url: string, description: string) {
+    const bookmark: Bookmark = {
+      id: uuid(),
+      url,
+      description,
+    };
+
+    this.bookmarks.push(bookmark);
+
+    return bookmark;
   }
 }
