c23903130394784660521fe92be11151ea16acbc
Babak Haj Azim Zanjani <babak.hajazimzanjani@gmail.com>
2021-07-10 22:55:27 +0200

    feat: deleteBookmark

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,15 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { url } from 'inspector';
 import { Bookmark } from './bookmark.model';
 import { BookmarksService } from './bookmarks.service';
 import { CreateBookmarkDto } from './dto/create-bookmark.dto';
+import { GetBookmarkDto } from './dto/get-bookmark.dto';
 
 @Controller('bookmarks')
 export class BookmarksController {
   constructor(private bookmarksService: BookmarksService) {}
 
   @Get()
-  findAll(): Bookmark[] {
+  find(@Query() getBookmarkDto: GetBookmarkDto): Bookmark[] {
+    if (Object.keys(getBookmarkDto).length) {
+      return this.bookmarksService.find(getBookmarkDto);
+    }
     return this.bookmarksService.findAll();
   }
 
@@ -21,5 +33,10 @@ export class BookmarksController {
   @Post()
   createBookmark(@Body() createBookmarkDto: CreateBookmarkDto): Bookmark {
     return this.bookmarksService.createBookmark(createBookmarkDto);
+  }
+
+  @Delete('/:id')
+  deleteBookmark(@Param('id') id: string): void {
+    return this.bookmarksService.deleteBookmark(id);
   }
 }
--- src/bookmarks/bookmarks.service.ts
+++ src/bookmarks/bookmarks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Bookmark } from './bookmark.model';
 import { v4 as uuid } from 'uuid';
 import { CreateBookmarkDto } from './dto/create-bookmark.dto';
+import { GetBookmarkDto } from './dto/get-bookmark.dto';
 
 @Injectable()
 export class BookmarksService {
@@ -9,6 +10,25 @@ export class BookmarksService {
 
   findAll(): Bookmark[] {
     return this.bookmarks;
+  }
+
+  find(getBookmarkDto: GetBookmarkDto): Bookmark[] {
+    let bookmarks = this.findAll();
+    const { url, description } = getBookmarkDto;
+
+    if (url) {
+      bookmarks = bookmarks.filter((bookmark) =>
+        bookmark.url.toLowerCase().includes(url),
+      );
+    }
+
+    if (description) {
+      bookmarks = bookmarks.filter((bookmark) =>
+        bookmark.description.toLowerCase().includes(description),
+      );
+    }
+
+    return bookmarks;
   }
 
   findById(id: string): Bookmark {
@@ -26,5 +46,9 @@ export class BookmarksService {
     this.bookmarks.push(bookmark);
 
     return bookmark;
+  }
+
+  deleteBookmark(id: string): void {
+    this.bookmarks = this.bookmarks.filter((bookmark) => bookmark.id !== id);
   }
 }
